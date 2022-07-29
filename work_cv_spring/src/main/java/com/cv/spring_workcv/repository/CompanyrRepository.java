a873e03c2ea64847cd8c41ff4e2ce4621f81ed64
LNTTTruc2712 <lenguyentrungtruc2712@gmail.com>
2022-07-29 19:04:00 +0700

    Update
    Update

---
+++ work_cv_spring/src/main/java/com/cv/spring_workcv/repository/CompanyrRepository.java
@@ -0,0 +1,32 @@
+package com.cv.spring_workcv.repository;
+
+import com.cv.spring_workcv.domain.Company;
+import com.cv.spring_workcv.domain.FollowCompany;
+import com.cv.spring_workcv.domain.User;
+import com.cv.spring_workcv.models.CompanyDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface CompanyrRepository extends PagingAndSortingRepository<Company, Integer> {
+
+    Company findCompanyByUser(User user);
+
+    @Query(value = "SELECT a.id as 'id',a.name_company as 'name_company',a.logo as 'logo',COUNT(b.id) as 'openPosition', a.phone_number as 'phoneNumber', a.email as 'email', a.address as 'address' FROM company as a, recruitment as b WHERE a.id = b.company_id GROUP BY b.company_id ORDER BY COUNT(b.id) DESC LIMIT 5",nativeQuery = true)
+    List<Object[]> getAll();
+
+    @Query(value = "SELECT a.id as 'id',a.name_company as 'name_company',a.logo as 'logo',COUNT(b.id) as 'openPosition', a.phone_number as 'phoneNumber', a.email as 'email', a.address as 'address' FROM company as a, recruitment as b WHERE a.id = b.company_id GROUP BY b.company_id",nativeQuery = true)
+    List<Object[]> getList();
+
+    Company findCompanyById(int id);
+
+    Company save(Company company);
+
+    List<Company> findAll();
+
+    Page<Company> findAll(Pageable pageable);
+}
---
+++ work_cv_spring/src/main/java/com/cv/spring_workcv/services/CompanyService.java
@@ -0,0 +1,30 @@
+package com.cv.spring_workcv.services;
+
+import com.cv.spring_workcv.domain.Company;
+import com.cv.spring_workcv.domain.Recruitment;
+import com.cv.spring_workcv.domain.User;
+import com.cv.spring_workcv.models.CompanyDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CompanyService {
+    Company getCompanyByUser(User user);
+
+    Company save(Company company);
+
+    List<Object[]> getAll();
+
+    List<Object[]> getList();
+
+    Company findCompanyById(int id);
+
+    List<Company> findAll();
+
+    Page<Company> findAll(Pageable pageable);
+
+}
